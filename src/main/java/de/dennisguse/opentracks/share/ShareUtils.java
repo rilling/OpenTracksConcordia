4ea4ac074acc1099a6452356b991472cd95d24da
Varun <varunaggarwal313@mail.com>
2023-10-09 15:00:39 -0400

    Fixed Issue#Reduce the total number of break and continue statements in this loop to use at most one

--- src/main/java/de/dennisguse/opentracks/share/ShareUtils.java
+++ src/main/java/de/dennisguse/opentracks/share/ShareUtils.java
@@ -95,7 +95,7 @@ public class ShareUtils {
             }
             if (marker.getPhotoURI() == null) {
                 Log.e(TAG, "MarkerId " + markerId.id() + " has no picture.");
-                continue;
+
             }
 
             mime = context.getContentResolver().getType(marker.getPhotoURI());
