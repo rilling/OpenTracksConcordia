0089bb800f0c40968c3cc504fc467ab48c63e113
nehahimane <116217770+nehahimane@users.noreply.github.com>
2023-10-10 15:54:04 -0400

    Fix var name to match regex

--- src/main/java/de/dennisguse/opentracks/stats/TrackStatistics.java
+++ src/main/java/de/dennisguse/opentracks/stats/TrackStatistics.java
@@ -355,11 +355,11 @@ public class TrackStatistics {
     }
 
     @VisibleForTesting(otherwise = VisibleForTesting.PACKAGE_PRIVATE)
-    public void addTotalAltitudeLoss(float loss_m) {
+    public void addTotalAltitudeLoss(float lossm) {
         if (totalAltitudeLoss_m == null) {
             totalAltitudeLoss_m = 0f;
         }
-        totalAltitudeLoss_m += loss_m;
+        totalAltitudeLoss_m += lossm;
     }
 
     @Override
