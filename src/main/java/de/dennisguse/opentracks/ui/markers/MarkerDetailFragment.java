5b7593164576332ebd03e9bde781a83a781ed522
Lovel George <lovelgeorge22@gmail.com>
2023-10-12 14:01:16 -0400

    Added comment explaining why this issue

--- src/main/java/de/dennisguse/opentracks/ui/markers/MarkerDetailFragment.java
+++ src/main/java/de/dennisguse/opentracks/ui/markers/MarkerDetailFragment.java
@@ -75,11 +75,15 @@ public class MarkerDetailFragment extends Fragment {
 
                 @Override
                 public void onAnimationStart(Animation anim) {
+                    // This method intentionally left empty as the onStart animation behavior is not required.
+
                     throw new UnsupportedOperationException("onAnimationStart is not supported for this class.");
                 }
 
                 @Override
                 public void onAnimationStart(Animation anim) {
+                    // This method intentionally left empty as the onStart animation behavior is not required.
+
                     throw new UnsupportedOperationException("onAnimationStart is not supported for this class.");
                 }
 
