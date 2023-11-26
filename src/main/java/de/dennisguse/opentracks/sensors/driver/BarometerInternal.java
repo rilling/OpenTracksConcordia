288ee4273f856bfb79e834fe303f146422f775fa
Rohan Dhiman <rohansarnjitdhiman@gmail.com>
2023-11-25 19:39:44 -0500

    Apply Advanced Filtering #243
    Implement Moving Average Filter in BarometerInternal

--- src/main/java/de/dennisguse/opentracks/sensors/driver/BarometerInternal.java
+++ src/main/java/de/dennisguse/opentracks/sensors/driver/BarometerInternal.java
@@ -13,6 +13,9 @@ import java.util.concurrent.TimeUnit;
 import de.dennisguse.opentracks.data.models.AtmosphericPressure;
 import de.dennisguse.opentracks.sensors.AltitudeSumManager;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BarometerInternal {
 
     private static final String TAG = BarometerInternal.class.getSimpleName();
@@ -20,6 +23,9 @@ public class BarometerInternal {
     private static final int SAMPLING_PERIOD = (int) TimeUnit.SECONDS.toMicros(5);
 
     private AltitudeSumManager observer;
+
+    private static final int FILTER_WINDOW_SIZE = 5; // Window size for the moving average filter
+    private Queue<Float> pressureReadings = new LinkedList<>();
 
     private final SensorEventListener listener = new SensorEventListener() {
         @Override
@@ -29,7 +35,22 @@ public class BarometerInternal {
                 return;
             }
 
-            observer.onSensorValueChanged(AtmosphericPressure.ofHPA(event.values[0]));
+            // Add new reading to the queue
+            if (pressureReadings.size() >= FILTER_WINDOW_SIZE) {
+                pressureReadings.poll(); // Remove the oldest reading
+            }
+            pressureReadings.offer(event.values[0]);
+
+            // Calculate the average of the readings in the queue
+            float sum = 0;
+            for (Float reading : pressureReadings) {
+                sum += reading;
+            }
+            float average = sum / pressureReadings.size();
+
+
+            // Use the averaged value instead of the raw reading
+            observer.onSensorValueChanged(AtmosphericPressure.ofHPA(average));
         }
 
         @Override
